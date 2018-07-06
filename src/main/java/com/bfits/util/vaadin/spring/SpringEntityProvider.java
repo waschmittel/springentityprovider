7d537b14ec8f8b5029ca2f02877eb2d6c3ecdb32
Daniel Flassak <daniel.flassak@bfits.com>
2018-07-06 16:17:19 +0200

    support Spring Boot 2.0 and fix #1

--- src/main/java/com/bfits/util/vaadin/spring/SpringEntityProvider.java
+++ src/main/java/com/bfits/util/vaadin/spring/SpringEntityProvider.java
@@ -1,11 +1,10 @@
 package com.bfits.util.vaadin.spring;
 
-import java.io.Serializable;
-import java.security.InvalidParameterException;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
+import com.bfits.util.vaadin.spring.SpringEntityProvider.ExampleFilter;
+import com.vaadin.data.provider.AbstractBackEndDataProvider;
+import com.vaadin.data.provider.ConfigurableFilterDataProvider;
+import com.vaadin.data.provider.Query;
+import com.vaadin.shared.data.sort.SortDirection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
@@ -14,11 +13,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.bfits.util.vaadin.spring.SpringEntityProvider.ExampleFilter;
-import com.vaadin.data.provider.AbstractBackEndDataProvider;
-import com.vaadin.data.provider.ConfigurableFilterDataProvider;
-import com.vaadin.data.provider.Query;
-import com.vaadin.shared.data.sort.SortDirection;
+import java.io.Serializable;
+import java.security.InvalidParameterException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Implement this so that Spring can properly autoinject.
@@ -56,40 +55,8 @@ import com.vaadin.shared.data.sort.SortDirection;
  */
 public abstract class SpringEntityProvider<T> extends AbstractBackEndDataProvider<T, ExampleFilter<T>> {
 
-    /**
-     * filter to filter by example
-     *
-     * @author FlassakD
-     *
-     * @param <T> type of filtered entity
-     */
-    public static final class ExampleFilter<T> {
-        private final Example<T> example;
-
-        /**
-         * create an example filter with the default matcher (ignore case and match strings with CONTAINING)
-         *
-         * @param probe Probe to filter by
-         */
-        public ExampleFilter(T probe) {
-            this(probe, ExampleMatcher.matching().withIgnoreCase().withStringMatcher(StringMatcher.CONTAINING));
-        }
-        // TODO: find out what the difference is between matching() matchingAll etc...
-
-        /**
-         * create an example filter with a custom matcher
-         *
-         * @param probe Probe to filter by
-         * @param exampleMatcher example matcher to use
-         */
-        public ExampleFilter(T probe, ExampleMatcher exampleMatcher) {
-            example = Example.of(probe, exampleMatcher);
-        }
-    }
-
     @Autowired
     private JpaRepository<T, Serializable> repository;
-
     private int resultLimit = 0;
 
     public SpringEntityProvider() {
@@ -105,7 +72,7 @@ public abstract class SpringEntityProvider<T> extends AbstractBackEndDataProvide
                                                                             sortOrder.getSorted()))
                                            .collect(Collectors.toList());
 
-        PageRequest pageRequest = new PageRequest(page, query.getLimit(), sortOrders.isEmpty() ? null : new Sort(sortOrders));
+        PageRequest pageRequest = PageRequest.of(page, query.getLimit(), sortOrders.isEmpty() ? Sort.unsorted() : Sort.by(sortOrders));
         List<T> items = null;
         if (query.getFilter().isPresent()) {
             items = repository.findAll(query.getFilter().get().example, pageRequest).getContent();
@@ -162,6 +129,36 @@ public abstract class SpringEntityProvider<T> extends AbstractBackEndDataProvide
         ConfigurableFilterDataProvider<T, Void, ExampleFilter<T>> filteredProvider = withConfigurableFilter();
         filteredProvider.setFilter(exampleFilter);
         return filteredProvider;
+    }
+
+    /**
+     * filter to filter by example
+     *
+     * @param <T> type of filtered entity
+     * @author FlassakD
+     */
+    public static final class ExampleFilter<T> {
+        private final Example<T> example;
+
+        /**
+         * create an example filter with the default matcher (ignore case and match strings with CONTAINING)
+         *
+         * @param probe Probe to filter by
+         */
+        public ExampleFilter(T probe) {
+            this(probe, ExampleMatcher.matching().withIgnoreCase().withStringMatcher(StringMatcher.CONTAINING));
+        }
+        // TODO: find out what the difference is between matching() matchingAll etc...
+
+        /**
+         * create an example filter with a custom matcher
+         *
+         * @param probe          Probe to filter by
+         * @param exampleMatcher example matcher to use
+         */
+        public ExampleFilter(T probe, ExampleMatcher exampleMatcher) {
+            example = Example.of(probe, exampleMatcher);
+        }
     }
 
 }
